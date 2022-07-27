b6aa3c9ec99244c7660b00c240ca8809d2af2135
misnsuk <fpqk1004@naver.com>
2022-07-27 12:20:04 +0900

    Test End

--- src/com/test/TestMain.java
+++ src/com/test/TestMain.java
@@ -1,9 +1,19 @@
 package com.test;
 
+import com.util.DBConnector;
+
 public class TestMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		DBConnector dbConnector = new DBConnector();
+		try {
+			dbConnector.getConnection();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 
 	}
 
